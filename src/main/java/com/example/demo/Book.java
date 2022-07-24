796d9eff40a99305ab3442605d2031aa0ee00039
gsmet <ludunyue@supcon.com>
2022-07-24 10:06:55 +0800

    added jdbc

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private float price;
+
+    protected Book(String title, String author, float price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    protected Book() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book [" + title + ", " + author + ", $" + price + "]";
+    }
+
+    // getters and setters...
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,94 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		String sql = "INSERT INTO books (title, author, price) VALUES (?, ?, ?)";
+//		int result = jdbcTemplate.update(sql, "Head First Java", "Kathy Sierra", 18.55f);
+//
+//		if (result > 0) {
+//			System.out.println("Insert successfully.");
+//		}
+
+//		SimpleJdbcInsert insertActor = new SimpleJdbcInsert(jdbcTemplate);
+//		insertActor.withTableName("books").usingGeneratedKeyColumns("id");
+//
+//		Book book = new Book("Effective Java", "Joshua Bloch", 29.99f);
+//		BeanPropertySqlParameterSource paramSource = new BeanPropertySqlParameterSource(book);
+//
+//		int result = insertActor.execute(paramSource);
+//
+//		if (result > 0) {
+//			System.out.println("Insert successfully.");
+//		}
+
+//		testUpdate();
+//		testListAll();
+//		testFind();
+	}
+
+	void testUpdate() {
+		String sql = "UPDATE books SET price=? WHERE title=?";
+		Object[] params = {9.99f, "Effective Java"};
+		int result = jdbcTemplate.update(sql, params);
+
+		if (result > 0) {
+			System.out.println("Update successfully.");
+		}
+	}
+
+	void testDelete(){
+		String sql = "DELETE FROM books WHERE title=?";
+		Object[] params = {"Effective Java"};
+		int result = jdbcTemplate.update(sql, params);
+
+		if (result > 0) {
+			System.out.println("Delete successfully.");
+		}
+	}
+
+	void testListAll() {
+		String sql = "SELECT * FROM books";
+
+		List<Book> listBooks = jdbcTemplate.query(sql,
+				BeanPropertyRowMapper.newInstance(Book.class));
+
+		for (Book book : listBooks) {
+			System.out.println(book);
+		}
+	}
+
+	void testFind() {
+		String sql = "SELECT * FROM books WHERE id = 1";
+		Book book = jdbcTemplate.queryForObject(sql,
+				BeanPropertyRowMapper.newInstance(Book.class));
+
+		System.out.println(book);
 	}
 
 	@GetMapping("/hello")
